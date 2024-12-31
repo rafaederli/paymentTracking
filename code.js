80a98ec84070ba95553443c346c4ca44c821d486
Rafael Ederli <rafaederli@gmail.com>
2024-12-31 12:48:18 -0300

    new features and refactoring

--- code.js
+++ code.js
@@ -1,9 +1,24 @@
+const url = "https://docs.google.com/spreadsheets/...";
+
 function doGet() {
-  var url = "https://docs.google.com/spreadsheets/d/1IhK1gK7rxyZ6UYoEm3ejQOebBxD-P1HT_djoZBnxKrQ/edit?gid=1768275107#gid=1768275107";
   var ss = SpreadsheetApp.openByUrl(url);
   var ws = ss.getSheetByName("beneficiarios");
-  var list = ws.getRange(1, 1, ws.getRange("A1").getDataRegion().getLastRow(), 1).getValues();
+  var payeeList = ws.getRange(1, 1, ws.getLastRow(), 1).getValues();
   template = HtmlService.createTemplateFromFile("main");
-  template.list = list.map(function(row) {return row[0]});
+  template.payeeList = payeeList.map(function(row) {return row[0]});
   return template.evaluate();
-}
+};
+
+function recordSpreadsheet(info) {
+  var ss = SpreadsheetApp.openByUrl(url);
+  
+  var ws = ss.getSheetByName("beneficiarios");
+  var payeeData = ws.getRange(1, 1, ws.getLastRow(), 2).getValues();
+  var payeeList = payeeData.map(function(row) {return row[0]});
+  var expenseList = payeeData.map(function(row) {return row[1]});
+  var payeeIndex = payeeList.indexOf(info.payee);
+  var expense = expenseList[payeeIndex];
+
+  var ws = ss.getSheetByName("contas a pagar");
+  ws.appendRow([info.payee, info.deliveryDate, info.merchandiseType, info.dueDate, info.value, "A Pagar", info.week, expense, new Date()]);
+};
--- script.js
+++ script.js
@@ -1,39 +1,151 @@
-// Delivery Date Validation
-const deliveryDateInput = document.getElementById("deliveryDate");
-const errorDeliveryDate = document.getElementById("errorDeliveryDate");
-deliveryDateInput.addEventListener("input", () => {
-    const deliveryDate = new Date(deliveryDateInput.value);
-    const today = new Date();
-    deliveryDate.setHours(24,0,0,0);
-    today.setHours(0,0,0,0);
-    if(deliveryDate < today) {
-        errorDeliveryDate.style.display = "block";
-    } else {
-        errorDeliveryDate.style.display = "none";
-    }
-});
-// Due Date Validation
-const dueDateInput = document.getElementById("dueDate");
-const errorDueDate = document.getElementById("errorDueDate");
-dueDateInput.addEventListener("input", () => {
-    const dueDate = new Date(dueDateInput.value);
-    const today = new Date();
-    dueDate.setHours(24,0,0,0);
-    today.setHours(0,0,0,0);
-    if(dueDate <= today) {
-        errorDueDate.style.display = "block";
-    } else {
-        errorDueDate.style.display = "none";
-    }
-});
-// Value Validation
-const valueInput = document.getElementById("value");
-valueInput.addEventListener("input", () => {
-    let value = valueInput.value;
-    value = value.replace(/\D/g,"");
-    const formattedValue = (Number(value)/100).toLocaleString("pt-BR", {
-        style: "currency",
-        currency: "BRL"
+    // validation: delivery date
+    const deliveryDate = document.getElementById("deliveryDate");
+    const errorDeliveryDate = document.getElementById("errorDeliveryDate");
+    
+    deliveryDate.addEventListener("input", () => {
+        const deliveryDateValue = new Date(deliveryDate.value);
+        deliveryDateValue.setHours(24,0,0,0);
+        
+        const today = new Date();
+        today.setHours(0,0,0,0);
+        
+        if(deliveryDateValue > today) {
+            errorDeliveryDate.style.display = "block";
+        } else {
+            errorDeliveryDate.style.display = "none";
+        }
     });
-    valueInput.value = formattedValue;
-});
+
+    // validation: due date
+    const dueDate = document.getElementById("dueDate");
+    const errorDueDate = document.getElementById("errorDueDate");
+
+    dueDate.addEventListener("input", () => {
+        const dueDateValue = new Date(dueDate.value);
+        dueDateValue.setHours(24,0,0,0);
+        
+        const today = new Date();
+        today.setHours(0,0,0,0);
+
+        if(dueDateValue <= today) {
+            errorDueDate.style.display = "block";
+        } else {
+            errorDueDate.style.display = "none";
+        }
+    });
+
+    // validation: value
+    const value = document.getElementById("value");
+    
+    value.addEventListener("input", () => {
+        const formattedValue = (
+          Number(value.value.replace(/\D/g,""))/100
+          ).toLocaleString("pt-BR", {style: "currency",currency:"BRL"});
+        
+        value.value = formattedValue;
+    });
+
+    // input to google spreadsheet
+    document.getElementById("button").addEventListener("click", getInfo);
+    
+    function getInfo() {
+      const fields = {
+        deliveryDate: document.getElementById("deliveryDate").value,
+        merchandiseType: document.getElementById("merchandiseType").value,
+        payee: document.getElementById("payee").value,
+        dueDate: document.getElementById("dueDate").value,
+        value: document.getElementById("value").value
+      };
+
+      var missingFields = []
+      for(var [field, value] of Object.entries(fields)) {
+        if(value == "" || value == "Selecione") {
+          switch(field) {
+              case "deliveryDate":
+              field = "Data da Entrega";
+              break;
+              case "merchandiseType":
+              field = "Tipo de Mercadoria";
+              break;
+              case "payee":
+              field = "Beneficiário";
+              break;
+              case "dueDate":
+              field = "Data do Vencimento";
+              break;
+              case "value":
+              field = "Valor";
+              break;
+            };
+          missingFields.push(field);
+        };
+      };
+
+      if(missingFields.length > 0) {
+        window.alert(`\nAtenção! Preencha os seguintes campos:\n\n · ${missingFields.join("\n · ")}`);
+      } else {
+      
+        var info = {
+          payee: fields.payee,
+          merchandiseType: fields.merchandiseType,
+          value: fields.value,
+        };
+
+        // transforming data fields
+        const dateFormat = {year: "numeric", month: "2-digit", day: "2-digit"};
+        
+        const deliveryDate = new Date(fields.deliveryDate);
+        deliveryDate.setHours(24,0,0,0);
+        info.deliveryDate = deliveryDate.toLocaleString("pt-BR", dateFormat);
+        
+        const dueDate = new Date(fields.dueDate);
+        dueDate.setHours(24,0,0,0);
+        info.dueDate = dueDate.toLocaleString("pt-BR", dateFormat);
+
+        // week field
+        const dayMilliseconds = (24 * 60 * 60 * 1000);
+        
+        const weekDay = dueDate.toLocaleString("pt-BR", {weekday: "long"});
+        let week;
+        const newDate = (n) => new Date(dueDate.getTime() - (n * dayMilliseconds));
+        
+        switch(weekDay) {
+          case "segunda-feira":
+            week = dueDate;
+            break;
+          case "terça-feira":
+            week = newDate(1);
+            break;
+          case "quarta-feira":
+            week = newDate(2);
+            break;
+          case "quinta-feira":
+            week = newDate(3);
+            break;
+          case "sexta-feira":
+            week = newDate(4);
+            break;
+          case "sábado":
+            week = newDate(5);
+            break;
+          case "domingo":
+            week = newDate(6);
+            break;
+        };
+
+        info.week = week.toLocaleString("pt-BR", dateFormat);
+        
+        // input
+        google.script.run.recordSpreadsheet(info);
+
+        // deleting field values
+        document.getElementById("deliveryDate").value = "";
+        document.getElementById("merchandiseType").value = "Selecione";
+        document.getElementById("payee").value = "Selecione";
+        document.getElementById("dueDate").value = "";
+        document.getElementById("value").value = "";
+        
+        document.getElementById("errorDeliveryDate").style.display = "none";
+        document.getElementById("errorDueDate").style.display = "none";
+      };
+    };
